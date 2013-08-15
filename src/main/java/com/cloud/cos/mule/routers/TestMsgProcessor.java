5688fc6ded979dd76bd8b96dfa8f24bf407fd9e4
linuxc <linuxc@126.com>
2013-08-15 15:51:41 +0800

    添加测试

---
+++ src/main/java/com/cloud/cos/mule/routers/TestMsgProcessor.java
@@ -0,0 +1,41 @@
+package com.cloud.cos.mule.routers;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.routing.AbstractAggregator;
+import org.mule.routing.correlation.CollectionCorrelatorCallback;
+import org.mule.routing.correlation.EventCorrelatorCallback;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cloud.cos.bean.ApiLocation;
+import com.cloud.cos.dao.ApiLocationDao;
+import com.cloud.cos.service.ResourceLocationService;
+
+public class TestMsgProcessor extends AbstractAggregator implements
+MessageProcessor {
+
+	private Logger log = Logger.getLogger(ResourceMsgProcessor.class);
+	
+	@Autowired
+	private ResourceLocationService resourceService;
+	
+	@Autowired
+	private ApiLocationDao apiLocationDao;
+	@Override
+	protected EventCorrelatorCallback getCorrelatorCallback(
+			MuleContext muleContext) {
+		EventCorrelatorCallback ecc=new CollectionCorrelatorCallback(muleContext, false, null);
+		return ecc;
+	}
+	
+	@Override
+	public MuleEvent process(MuleEvent event) throws MuleException {
+		List<ApiLocation> list = apiLocationDao.getAllList();
+		return event;
+	}
+}
