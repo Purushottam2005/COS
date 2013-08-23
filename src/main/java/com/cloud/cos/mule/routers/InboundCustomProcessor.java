14f5c4996b9c48509f034770bff276948816f6c1
wangjf <wangjf@wangjf-PC>
2013-08-23 16:58:32 +0800

    12

--- src/main/java/com/cloud/cos/mule/routers/InboundCustomProcessor.java
+++ src/main/java/com/cloud/cos/mule/routers/InboundCustomProcessor.java
@@ -1,15 +1,26 @@
 package com.cloud.cos.mule.routers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.mule.api.MuleContext;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleException;
+import org.mule.api.MuleMessage;
 import org.mule.api.processor.MessageProcessor;
+import org.mule.module.client.MuleClient;
+import org.mule.routing.AbstractAggregator;
+import org.mule.routing.correlation.CollectionCorrelatorCallback;
+import org.mule.routing.correlation.EventCorrelatorCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cloud.cos.bean.ResourceLocation;
 import com.cloud.cos.service.ResourceLocationService;
 
-public class InboundCustomProcessor implements MessageProcessor{
+public class InboundCustomProcessor extends AbstractAggregator implements MessageProcessor{
 	
 	private Logger log = Logger.getLogger(InboundCustomProcessor.class);
 
@@ -27,7 +38,26 @@ public class InboundCustomProcessor implements MessageProcessor{
 		System.out.println("port:                      " + resourceLocation.getPort());
 		System.out.println("protocol:                  " + resourceLocation.getProtocol());
 		System.out.println("###################################");
-		return null;
+		
+		List<Object> payloadList = new ArrayList<Object>();
+		MuleMessage msg = event.getMessage();
+		MuleClient client = new MuleClient(muleContext);
+		Map<String, String> props = new HashMap<String, String>();
+		props.put("http.method", "PUT");
+		MuleMessage result = client.send("http://" + resourceLocation.getHost()
+				+ ":" + resourceLocation.getPort() + resourceLocation.getName(),
+				msg.getPayload(), props);
+		 log.info("the one client message:" + result);
+		payloadList.add(result.getPayload(String.class));
+		return event;
+	}
+
+	@Override
+	protected EventCorrelatorCallback getCorrelatorCallback(
+			MuleContext muleContext) {
+		EventCorrelatorCallback ecc = new CollectionCorrelatorCallback(
+				muleContext, false, null);
+		return ecc;
 	}
 
 }
